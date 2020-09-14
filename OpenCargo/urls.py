cb9aa3ef30c8ba95afef7a95e9c726ffdd55255f
Bruno <brunofernandez@protonmail.com>
2020-09-14 00:54:09 -0300

    se logro que cambien los css e imagenes pero queda horrible si solo se mete en el html y no queda bien organizado

--- OpenCargo/urls.py
+++ OpenCargo/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from OpenCargo.views import home
+from OpenCargo.views import home, preguntas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home)
+    path('home/', home),
+    path('PreguntasFrecuentes/', preguntas)
 ]
--- OpenCargo/views.py
+++ OpenCargo/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html")
+
+def preguntas(request):
+    return render(request, "preguntas.html")
