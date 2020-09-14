ef143b34520eaf50d677c013a71af6f34a9e80f1
Bruno <brunofernandez@protonmail.com>
2020-09-14 14:37:12 -0300

    se agrego la aplicacion registrarCliente

--- OpenCargo/settings.py
+++ OpenCargo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'OpenCargo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'open_cargo',
+        'USER': 'postgres',
+        'PASSWORD': 'AhqvmLGH',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': 5432,
     }
 }
 
