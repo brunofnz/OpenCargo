a4238332d940d3669b9721340d8afff04a6e5cb4
Bruno <brunofernandez@protonmail.com>
2020-09-28 02:48:29 -0300

    agregue prueba de cliente

--- apps/registrar/models.py
+++ apps/registrar/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    ide = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length= 200, blank=False, null=False)
+    apellido = models.CharField(max_length= 200, blank=False, null=False)
+    nacionalidad = models.CharField(max_length= 200, blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        ordering = ['apellido']
+
+    def __str__(self):
+        return "{}, {}".format(self.apellido,self.nombre)
+
+        
+    
